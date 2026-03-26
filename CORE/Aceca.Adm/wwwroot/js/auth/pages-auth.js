7ee126a22d47ee1a63d66f82b04f55485415e988
Rodrigo Silva <rodrigo.silva@annexus.com.br>
2026-03-26 08:22:25 -0300

    lembre me

--- CORE/Aceca.Adm/wwwroot/js/auth/pages-auth.js
+++ CORE/Aceca.Adm/wwwroot/js/auth/pages-auth.js
@@ -256,3 +256,28 @@ async function fn_Auth(e) {
         });
     }
 }
+
+/*
+function fn() {
+    const rmCheck = document.getElementById("rememberMe"),
+        emailInput = document.getElementById("email");
+
+    if (localStorage.checkbox && localStorage.checkbox !== "") {
+        rmCheck.setAttribute("checked", "checked");
+        emailInput.value = localStorage.username;
+    } else {
+        rmCheck.removeAttribute("checked");
+        emailInput.value = "";
+    }
+}
+function lsLembrarMe() {
+    if (rmCheck.checked && emailInput.value !== "") {
+        localStorage.username = emailInput.value;
+        localStorage.checkbox = rmCheck.value;
+    } else {
+        localStorage.username = "";
+        localStorage.checkbox = "";
+    }
+}
+
+*/
